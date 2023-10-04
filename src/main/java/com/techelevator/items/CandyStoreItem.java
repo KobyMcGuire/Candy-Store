1057a0ba0251593f2fa02c1bd6ee1c257eb826f4
Steve Carmichael <steve@techelevator.com>
2023-10-04 09:52:08 -0400

    added capstone starter code

---
+++ src/main/java/com/techelevator/items/CandyStoreItem.java
@@ -0,0 +1,9 @@
+package com.techelevator.items;
+
+/*
+    This represents a single candy store item in the system
+
+    This is an abstract class that should be used as a superclass for the more specific items.
+ */
+public abstract class CandyStoreItem {
+}
